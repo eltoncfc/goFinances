a00f479f694247b46a90c80d1058c9320abc9d62
eltoncfc <elton.martins@live.com>
2023-02-07 20:24:40 -0300

    initial commit

---
+++ src/componentes/HighlightCard/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import {
+  Container,
+  Header,
+  Title,
+  Icon,
+  Footer,
+  Amount,
+  LastTransaction,
+} from "./styles";
+
+interface props {
+  title: string;
+  amount: string;
+  lastTransaction: string;
+  type: "up" | "down" | "total";
+}
+
+const icon = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+  total: "dollar-sign",
+};
+
+export function HighlightCard({ type, title, amount, lastTransaction }: props) {
+  return (
+    <Container type={type}>
+      <Header>
+        <Title type={type}> {title}</Title>
+        <Icon name={icon[type]} type={type} />
+      </Header>
+
+      <Footer>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>{lastTransaction}</LastTransaction>
+      </Footer>
+    </Container>
+  );
+}
+
+export default HighlightCard;
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import { HighlightCard } from "../../componentes/HighlightCard";
+import { TransactionCard } from "../../componentes/TransactionCard";
+
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
+} from "./styles";
+
+const data = [
+  {
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+      name: "Vendas",
+      icon: "dollar-sign",
+    },
+    date: "13/04/2020",
+  },
+  {
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+      name: "Vendas",
+      icon: "dollar-sign",
+    },
+    date: "13/04/2020",
+  },
+  {
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+      name: "Vendas",
+      icon: "dollar-sign",
+    },
+    date: "13/04/2020",
+  },
+];
+export default function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/60990228?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Elton</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última entrada dia 3 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: getBottomSpace(),
+          }}
+        />
+      </Transactions>
+    </Container>
+  );
+}
